8a25baf1f767aca5ccb5b987df6ff4fa2b33a070
Sven Wappler <info@wappler.systems>
2020-10-12 01:41:49 +0200

    Info text example

--- ext_tables.php
+++ ext_tables.php
@@ -10,6 +10,15 @@ call_user_func(
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_wsslider_domain_model_item');
 
 
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+            'tt_content.pi_flexform.ws_slider',
+            'EXT:ws_slider/Resources/Private/Language/flexslider_csh_flexforms.xlf'
+        );
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+            'tt_content.pi_flexform.ws_slider',
+            'EXT:ws_slider/Resources/Private/Language/owl_csh_flexforms.xlf'
+        );
+
     },
     'ws_slider'
 );
