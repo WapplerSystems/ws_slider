d4eaf39ce1d138b93d8122615a9e1376c852afaa
Sven Wappler <wappler@wappler.systems>
2026-04-09 14:29:41 +0200

    Make FlexFormService standalone (FlexFormTools is readonly in v14)

--- Classes/FlexForm/FlexFormService.php
+++ Classes/FlexForm/FlexFormService.php
@@ -3,24 +3,19 @@ declare(strict_types=1);
 
 namespace WapplerSystems\WsSlider\FlexForm;
 
-
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-class FlexFormService extends \TYPO3\CMS\Core\Service\FlexFormService
+/**
+ * Standalone FlexForm parser with bugfix for nested key TypeError.
+ * Cannot extend FlexFormTools in v14 (readonly class).
+ */
+class FlexFormService
 {
-
     /**
-     * Parses the flexForm content and converts it to an array
-     * The resulting array will be multi-dimensional, as a value "bla.blubb"
-     * results in two levels, and a value "bla.blubb.bla" results in three levels.
-     *
-     * Note: multi-language flexForms are not supported yet
-     *
-     * @param string $flexFormContent flexForm xml string
-     * @param string $languagePointer language pointer used in the flexForm
-     * @param string $valuePointer value pointer used in the flexForm
+     * Parses the flexForm content and converts it to an array.
+     * Includes a fix for TypeError when traversing nested dotted keys.
      */
-    public function convertFlexFormContentToArray($flexFormContent, $languagePointer = 'lDEF', $valuePointer = 'vDEF'): array
+    public function convertFlexFormContentToArray(string $flexFormContent, string $languagePointer = 'lDEF', string $valuePointer = 'vDEF'): array
     {
         $settings = [];
         $flexFormArray = GeneralUtility::xml2array($flexFormContent);
@@ -56,4 +51,42 @@ class FlexFormService extends \TYPO3\CMS\Core\Service\FlexFormService
         return $settings;
     }
 
+    private function walkFlexFormNode(mixed $nodeArray, string $valuePointer = 'vDEF'): mixed
+    {
+        if (!is_array($nodeArray)) {
+            return $nodeArray;
+        }
+        $result = [];
+        foreach ($nodeArray as $nodeKey => $nodeValue) {
+            if ($nodeKey === $valuePointer) {
+                return $nodeValue;
+            }
+            if (in_array($nodeKey, ['el', '_arrayContainer'])) {
+                return $this->walkFlexFormNode($nodeValue, $valuePointer);
+            }
+            if (($nodeKey[0] ?? '') === '_') {
+                continue;
+            }
+            if (strpos((string)$nodeKey, '.')) {
+                $nodeKeyParts = explode('.', $nodeKey);
+                $currentNode = &$result;
+                $nodeKeyPartsCount = count($nodeKeyParts);
+                for ($i = 0; $i < $nodeKeyPartsCount - 1; $i++) {
+                    $currentNode = &$currentNode[$nodeKeyParts[$i]];
+                }
+                $newNode = [next($nodeKeyParts) => $nodeValue];
+                $subVal = $this->walkFlexFormNode($newNode, $valuePointer);
+                $currentNode[key($subVal)] = current($subVal);
+            } elseif (is_array($nodeValue)) {
+                if (array_key_exists($valuePointer, $nodeValue)) {
+                    $result[$nodeKey] = $nodeValue[$valuePointer];
+                } else {
+                    $result[$nodeKey] = $this->walkFlexFormNode($nodeValue, $valuePointer);
+                }
+            } else {
+                $result[$nodeKey] = $nodeValue;
+            }
+        }
+        return $result;
+    }
 }
